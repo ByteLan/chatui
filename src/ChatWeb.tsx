044b5063c6a1e206cbc51b2b71a0c79d991bc847
bytelan <bytelan@163.com>
2025-02-21 00:06:53 +0800

    完善会话管理，加入mdx支持

--- src/ChatWeb.tsx
+++ src/ChatWeb.tsx
@@ -1,6 +1,8 @@
 import FullChatApp from './FullChatApp.tsx'
 import {useRef, useState} from "react";
 import { Splitter } from "antd";
+import {IllustrationNoContent, IllustrationNoContentDark} from "@douyinfe/semi-illustrations";
+import {Empty} from "@douyinfe/semi-ui";
 
 
 
@@ -9,7 +11,20 @@ export default function ChatWeb() {
     const [chatSize, setChatSize] = useState<number|string>('100%');
     //使用useRef，类似于创造一个全局变量
     const chatRef = useRef();
-    const [rightNode, setRightNode] = useState(<iframe src = "https://www.bytelan.cn" width="100%" height="100%"></iframe>);
+    const [rightNode, setRightNode] = useState(
+        <Empty image={<IllustrationNoContent style={{ width: 300 }} />}
+               darkModeImage={<IllustrationNoContentDark style={{ width: 300 }} />}
+               title="多模态交互页">
+            <div style={{width: 400 }}>
+                <p>这里是多模态交互页，您可以通过左右页面之间的分隔栏调整大小。</p>
+                <p>左侧为多智能体 Chat 界面，右侧为多模态交互界面。</p>
+                <h3>交互说明</h3>
+                <p>当您在左侧 Chat 中触发特定功能，可以在右侧展开对应交互界面。</p>
+                <p>  例如：当您在 Chat 中输入“生成供应链仿真环境”，智能体会向您返回打开右侧仿真的按钮。</p>
+                <p>您在右侧使用的功能，会在必要时向您左侧的 Chat 中发送分析结果等信息，请留意。</p>
+            </div>
+
+        </Empty>);
 
     const onSplitterSizeChange = (sizes: number[]) => {
         console.warn("onSplitterSizeChange: "+sizes);
